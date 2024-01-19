5c5f587ab10d14effff1744c1a27dede2e816112
Bochkovskyi <unundertale1@ukr.net>
2024-01-19 20:12:57 +0200

    add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+import { classnames } from '@/utils'
+import Menu from './Menu/Menu'
+import MenuItem from './Menu/MenuItem'
+import MenuIcon from './Menu/MenuIcon/MenuIcon'
+
+type HeaderProps = {
+  className?: string
+}
+
+const Header: FC<HeaderProps> = ({ className }) => {
+  return (
+    <header
+      className={
+        (classnames(
+          'px-8 lg:px-10 py-4 lg:py-6 fixed top-0 left-0 w-full bg-gray'
+        ),
+        className)
+      }
+    >
+      <Menu className="mx-auto">
+        <MenuItem href="/">Home</MenuItem>
+        <MenuItem href="/#about-me">About Me</MenuItem>
+        <MenuItem href="/#my-projects">My Projects</MenuItem>
+        <MenuItem href="/#contact-me">Contact Me</MenuItem>
+      </Menu>
+      <MenuIcon />
+    </header>
+  )
+}
+
+export default Header
