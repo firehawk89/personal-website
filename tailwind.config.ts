5ebd8d60c8c9d2206101d0eb984dd52055866546
Bochkovskyi <unundertale1@ukr.net>
2024-01-19 19:52:58 +0200

    add new tailwind animations classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,45 @@ const config: Config = {
         gray: '#f5f5f5',
         accent: '#ff5100',
       },
+      keyframes: {
+        'top-down': {
+          '0%': { top: '0', transform: 'rotate(0)' },
+          '50%': { top: '10px', transform: 'rotate(0)' },
+          '100%': { top: '10px', transform: 'rotate(45deg)' },
+        },
+        'top-up': {
+          '0%': { top: '10px', transform: 'rotate(45deg)' },
+          '50%': { top: '10px', transform: 'rotate(0)' },
+          '100%': { top: '0', transform: 'rotate(0)' },
+        },
+        'bottom-down': {
+          '0%': { bottom: '0', transform: 'rotate(0)' },
+          '50%': { bottom: '10px', transform: 'rotate(0)' },
+          '100%': { bottom: '10px', transform: 'rotate(135deg)' },
+        },
+        'bottom-up': {
+          '0%': { bottom: '10px', transform: 'rotate(135deg)' },
+          '50%': { bottom: '10px', transform: 'rotate(0)' },
+          '100%': { bottom: '0', transform: 'rotate(0)' },
+        },
+        scaled: {
+          '0%': { transform: 'scale(0)' },
+          '50%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        'scaled-none': {
+          '50%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(0)' },
+        },
+      },
+      animation: {
+        'top-down': 'top-down 0.5s ease forwards',
+        'top-up': 'top-up 0.5s ease forwards',
+        'bottom-down': 'bottom-down 0.5s ease forwards',
+        'bottom-up': 'bottom-up 0.5s ease forwards',
+        scaled: 'scaled 0.5s ease forwards',
+        'scaled-none': 'scaled-none 0.5s ease forwards',
+      },
     },
   },
   plugins: [],
