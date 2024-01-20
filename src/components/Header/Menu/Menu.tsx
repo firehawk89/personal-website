48bc378a2fc009bb7b72572dabb385ab57c78e3c
Bochkovskyi <unundertale1@ukr.net>
2024-01-20 21:13:55 +0200

    utilize isMenuOpened from HeaderContext

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -1,4 +1,7 @@
-import { FC, ReactNode } from 'react'
+'use client'
+
+import { FC, ReactNode, useContext } from 'react'
+import HeaderContext from '@/store/header/header-context'
 import { classnames } from '@/utils'
 
 type MenuProps = {
@@ -7,8 +10,18 @@ type MenuProps = {
 }
 
 const Menu: FC<MenuProps> = ({ children, className }) => {
+  const { isMenuOpened } = useContext(HeaderContext)
+
   return (
-    <ul className={classnames('flex w-fit gap-10', className)}>{children}</ul>
+    <ul
+      className={classnames(
+        'py-8 md:p-0 fixed md:static z-20 top-0 left-0 md:translate-y-0 w-full md:w-fit flex flex-col md:flex-row justify-center md:justify-stretch items-center md:items-stretch gap-6 md:gap-10 bg-gray md:bg-transparent transition-transform duration-500',
+        isMenuOpened ? 'translate-y-0' : '-translate-y-full',
+        className
+      )}
+    >
+      {children}
+    </ul>
   )
 }
 
