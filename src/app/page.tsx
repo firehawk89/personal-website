c29b62173ff20dfee5f6127ebb3d66277845db0b
Bochkovskyi <unundertale1@ukr.net>
2024-01-21 13:08:51 +0200

    add lorem text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>MAIN</div>
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque adipisci
+      eveniet dicta dolor quod odio natus perferendis saepe est tempore modi
+      tenetur ullam ex quisquam repellendus voluptate, ipsam, nam ad? Lorem
+      ipsum, dolor sit amet consectetur adipisicing elit. Neque adipisci eveniet
+      dicta dolor quod odio natus perferendis saepe est tempore modi tenetur
+      ullam ex quisquam repellendus voluptate, ipsam, nam ad? Lorem ipsum, dolor
+      sit amet consectetur adipisicing elit. Neque adipisci eveniet dicta dolor
+      quod odio natus perferendis saepe est tempore modi tenetur ullam ex
+      quisquam repellendus voluptate, ipsam, nam ad? Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Neque adipisci eveniet dicta dolor quod odio
+      natus perferendis saepe est tempore modi tenetur ullam ex quisquam
+      repellendus voluptate, ipsam, nam ad?
+    </div>
+  )
 }
