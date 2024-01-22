83ad5e2edcedc7183572bd695e143c2a1f97ad8b
Bochkovskyi <unundertale1@ukr.net>
2024-01-22 20:49:22 +0200

    update styles, remove Home link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,14 +12,13 @@ const Header: FC<HeaderProps> = ({ className }) => {
   return (
     <header
       className={classnames(
-        'px-8 lg:px-10 py-5 fixed top-0 left-0 w-full bg-light bg-opacity-10 shadow-blur backdrop-blur-[8px]',
+        'px-8 lg:px-10 py-5 fixed z-10 top-0 left-0 w-full bg-light bg-opacity-10 shadow-blur backdrop-blur-[8px]',
         className
       )}
     >
       <Menu className="mx-auto">
-        <MenuItem href="/">Home</MenuItem>
         <MenuItem href="/#about-me">About Me</MenuItem>
-        <MenuItem href="/#my-projects">My Projects</MenuItem>
+        <MenuItem href="/#my-projects">My Work</MenuItem>
         <MenuItem href="/#contact-me">Contact Me</MenuItem>
       </Menu>
       <MenuIcon className="md:hidden ml-auto" />
