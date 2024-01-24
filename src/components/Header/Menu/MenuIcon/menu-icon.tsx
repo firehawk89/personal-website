c8df1d5f0e6720c4e450fbac982d622d1a755eb1
Bochkovskyi <unundertale1@ukr.net>
2024-01-24 14:27:51 +0200

    rename menuIcon component file

---
+++ src/components/Header/Menu/MenuIcon/menu-icon.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { FC, useContext } from 'react'
+import HeaderContext from '@/store/header-context'
+import { classnames } from '@/utils'
+import MenuIconBar from './menu-icon-bar'
+
+type MenuIconProps = {
+  className?: string
+}
+
+const MenuIcon: FC<MenuIconProps> = ({ className }) => {
+  const { isMenuOpened, toggleMenu } = useContext(HeaderContext)
+
+  return (
+    <div
+      className={classnames(
+        'cursor-pointer relative z-30 w-7 flex flex-col gap-2',
+        className
+      )}
+      onClick={toggleMenu}
+    >
+      <MenuIconBar
+        className={isMenuOpened ? 'animate-top-down' : 'animate-top-up'}
+      />
+      <MenuIconBar
+        className={isMenuOpened ? 'animate-scaled-none' : 'animate-scaled'}
+      />
+      <MenuIconBar
+        className={isMenuOpened ? 'animate-bottom-up' : 'animate-bottom-down'}
+      />
+    </div>
+  )
+}
+
+export default MenuIcon
