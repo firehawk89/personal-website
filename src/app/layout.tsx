a83a1d484c1f43ba25829a0103336cb03609bb7e
Bochkovskyi <unundertale1@ukr.net>
2024-01-26 20:07:57 +0200

    add suppresHydrationWarning rule

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={classnames('flex min-h-screen flex-col', raleway.className)}
       >
