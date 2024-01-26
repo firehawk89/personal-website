f333f3455d7b047b4760e7cf4fa8a90fa57255f4
Bochkovskyi <unundertale1@ukr.net>
2024-01-26 15:42:09 +0200

    add theme-toggler component

---
+++ src/components/ui/theme-toggler.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Button from '@/components/ui/button'
+import Theme from '@/types/enums/Theme'
+import { useTheme } from 'next-themes'
+import { FC, useEffect, useState } from 'react'
+import { CiDark, CiLight } from 'react-icons/ci'
+
+type ThemeTogglerProps = { className?: string }
+
+const ThemeToggler: FC<ThemeTogglerProps> = ({ className }) => {
+  const [mounted, setMounted] = useState<boolean>(false)
+  const { resolvedTheme, setTheme } = useTheme()
+  const isDarkTheme = (resolvedTheme as Theme) === 'dark'
+
+  const toggleTheme = () => (isDarkTheme ? setTheme('light') : setTheme('dark'))
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) {
+    return (
+      <Button className={className} variant="icon">
+        <div className="w-6 h-6 rounded-full bg-gray-300 animate-pulse" />
+      </Button>
+    )
+  }
+
+  return (
+    <Button className={className} onClick={toggleTheme} variant="icon">
+      {isDarkTheme ? (
+        <CiDark className="w-6 h-6" />
+      ) : (
+        <CiLight className="w-6 h-6" />
+      )}
+    </Button>
+  )
+}
+
+export default ThemeToggler
