5ed3b4fe0beda4de714d991075d64dbb83d4f60f
Bochkovskyi <unundertale1@ukr.net>
2024-02-02 18:26:44 +0200

    utilize Heading component

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/ui/button'
 import Content from '@/components/ui/content'
+import Heading from '@/components/ui/heading'
 import Socials from '@/components/ui/socials/socials'
 import { cn } from '@/utils'
 import { FC } from 'react'
@@ -13,9 +14,9 @@ const Hero: FC = () => {
     >
       <Content className="flex flex-col items-center justify-center text-center">
         <article className="max-w-4xl">
-          <h1 className="text-4xl font-bold md:text-5xl">
+          <Heading className="mx-auto">
             Hi, I&apos;m <span className="text-accent">Anton Bochkovskyi</span>
-          </h1>
+          </Heading>
           <p className="mt-4 font-medium md:text-xl">
             Motivated Front-End Developer with a passion for creating engaging,
             elegant and responsive user interfaces.
