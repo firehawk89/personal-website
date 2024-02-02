5b59eade8ed59f0285c4cf7a4af3b56ddbc8ca3f
Bochkovskyi <unundertale1@ukr.net>
2024-02-02 18:27:32 +0200

    utilize About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import About from '@/components/sections/about'
 import Hero from '@/components/sections/hero/hero'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  )
 }
