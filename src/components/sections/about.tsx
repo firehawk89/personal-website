e256385460e6039ef0f5806f4a78e11cf50aef93
Bochkovskyi <unundertale1@ukr.net>
2024-02-02 18:27:07 +0200

    add about section

---
+++ src/components/sections/about.tsx
@@ -0,0 +1,46 @@
+import Content from '@/components/ui/content'
+import Heading from '@/components/ui/heading'
+import { cn } from '@/utils'
+import { FC } from 'react'
+
+const About: FC = () => {
+  return (
+    <section
+      className={cn('bg-light py-14 dark:bg-black md:py-20')}
+      id="about-me"
+    >
+      <Content className="flex flex-col items-center justify-center">
+        <article>
+          <Heading size="h2" variant="underline">
+            About Me
+          </Heading>
+          <div className="mt-6 space-y-4 font-medium md:text-xl">
+            <p>
+              I&apos;m a <strong>Front-End Developer</strong> specializing in
+              React, Next.js, and Tailwind CSS, with a keen focus on crafting{' '}
+              <b>responsive</b> and visually <b>stunning</b> web applications.
+            </p>
+            <p>
+              I enjoy working closely with cross-functional teams, including
+              designers and back-end developers, to{' '}
+              <strong>transform ideas into reality</strong>. Clear
+              communication, attention to detail, and a commitment to delivering{' '}
+              <b>high-quality</b> code are values I uphold in my works.
+            </p>
+            <p>
+              If you&apos;re in search of a developer who is <b>well-versed</b>{' '}
+              in React, Next.js, and Tailwind CSS, and has a passion for
+              crafting <b>responsive</b> and <b>user-centric</b> applications,
+              I&apos;m eager to contribute my skills to your projects.
+              Let&apos;s collaborate and build digital experiences that leave a{' '}
+              <b>lasting impression</b>!
+            </p>
+          </div>
+        </article>
+        <div></div>
+      </Content>
+    </section>
+  )
+}
+
+export default About
