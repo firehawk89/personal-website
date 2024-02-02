34f1fe74718c2d84b036d8b69552c2b08e38330a
Bochkovskyi <unundertale1@ukr.net>
2024-02-02 20:34:10 +0200

    add TechnologiesItem component

---
+++ src/components/ui/technologies/technologies-item.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/utils'
+import { ComponentType, FC, HTMLAttributes } from 'react'
+import { IconBaseProps } from 'react-icons'
+
+interface TechnologiesItemProps extends HTMLAttributes<HTMLLIElement> {
+  Icon: ComponentType<IconBaseProps>
+  title: string
+}
+
+const TechnologiesItem: FC<TechnologiesItemProps> = ({
+  Icon,
+  className,
+  title,
+  ...props
+}) => {
+  return (
+    <li
+      className={cn('h-fit w-fit overflow-hidden rounded-md', className)}
+      {...props}
+    >
+      <Icon
+        aria-hidden="true"
+        className="h-16 w-16 md:h-24 md:w-24"
+        focusable="false"
+      />
+      <span className="sr-only">{title}</span>
+    </li>
+  )
+}
+
+export default TechnologiesItem
