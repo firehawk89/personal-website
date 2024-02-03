a14d0c17adbd74a6efa6cfcecc1a87d30c8c41e6
Bochkovskyi <unundertale1@ukr.net>
2024-02-03 21:34:12 +0200

    add position variants

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -4,16 +4,21 @@ import { FC, HTMLAttributes } from 'react'
 
 export const headingVariants = cva('w-fit', {
   defaultVariants: {
+    position: 'default',
     size: 'h1',
   },
   variants: {
+    position: {
+      center: 'mx-auto after:left-1/2 after:-translate-x-1/2',
+      default: 'after:left-0',
+    },
     size: {
       h1: 'text-4xl font-bold md:text-5xl',
       h2: 'text-3xl font-semibold md:text-4xl',
     },
     variant: {
       underline:
-        'pb-1.5 relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:bg-accent after:w-2/6',
+        'pb-1.5 relative after:absolute after:bottom-0 after:h-0.5 after:bg-accent after:w-2/6',
     },
   },
 })
@@ -22,11 +27,19 @@ interface HeadingProps
   extends HTMLAttributes<HTMLHeadingElement>,
     VariantProps<typeof headingVariants> {}
 
-const Heading: FC<HeadingProps> = ({ children, className, size, variant }) => {
+const Heading: FC<HeadingProps> = ({
+  children,
+  className,
+  position,
+  size,
+  variant,
+}) => {
   const HeadingTag = size ? size : 'h1'
 
   return (
-    <HeadingTag className={cn(headingVariants({ size, variant }), className)}>
+    <HeadingTag
+      className={cn(headingVariants({ position, size, variant }), className)}
+    >
       {children}
     </HeadingTag>
   )
