0a3e3e2ca0f27df806e308cac9ed958906559b51
Bochkovskyi <unundertale1@ukr.net>
2024-02-07 16:59:55 +0200

    utilize Contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from '@/components/sections/about'
+import Contact from '@/components/sections/contact/contact'
 import Hero from '@/components/sections/hero/hero'
 import Projects from '@/components/sections/projects/projects'
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About className="py-16 md:py-24" id="about-me" />
       <Projects className="py-16 md:py-24" id="my-projects" />
+      <Contact className="py-16 md:py-24" id="contact-me" />
     </>
   )
 }
