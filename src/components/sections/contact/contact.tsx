2dd54f43605235e41975124e5f34d3766c4ffe77
Bochkovskyi <unundertale1@ukr.net>
2024-02-08 14:46:26 +0200

    update styles

--- src/components/sections/contact/contact.tsx
+++ src/components/sections/contact/contact.tsx
@@ -9,7 +9,7 @@ interface ContactProps extends HTMLAttributes<HTMLDivElement> {}
 const Contact: FC<ContactProps> = ({ className, ...props }) => {
   return (
     <section className={cn('bg-light dark:bg-black', className)} {...props}>
-      <Content className="max-w-5xl">
+      <Content className="max-w-4xl">
         <Heading size="h2" variant="underline">
           Get In Touch
         </Heading>
