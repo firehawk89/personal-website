6afdd628cf664ee915c6c39797e4699ad3cea7ce
Bochkovskyi <unundertale1@ukr.net>
2024-02-11 18:27:04 +0200

    add ChipsItem component

---
+++ src/components/ui/chips/chips-item.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils'
+import { VariantProps, cva } from 'class-variance-authority'
+import { FC, LiHTMLAttributes } from 'react'
+
+const chipsItemVariants = cva(
+  'bg-accent bg-opacity-75 font-semibold text-light',
+  {
+    defaultVariants: {
+      size: 'default',
+    },
+    variants: {
+      size: {
+        default: 'rounded-2xl px-4 py-1.5',
+        sm: 'rounded-xl px-3 py-1 text-sm',
+      },
+    },
+  }
+)
+
+interface ChipsItemProps
+  extends LiHTMLAttributes<HTMLLIElement>,
+    VariantProps<typeof chipsItemVariants> {
+  label: string
+}
+
+const ChipsItem: FC<ChipsItemProps> = ({ className, label, size }) => {
+  return <li className={cn(chipsItemVariants({ size }), className)}>{label}</li>
+}
+
+export default ChipsItem
