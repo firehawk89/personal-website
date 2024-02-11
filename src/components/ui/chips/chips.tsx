af1a5c050b01aaf5309b1b3b5f74b9c7de971d0e
Bochkovskyi <unundertale1@ukr.net>
2024-02-12 00:00:34 +0200

    add interface export

--- src/components/ui/chips/chips.tsx
+++ src/components/ui/chips/chips.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/utils'
 import { FC, HTMLAttributes } from 'react'
 
-interface ChipsProps extends HTMLAttributes<HTMLUListElement> {}
+export interface ChipsProps extends HTMLAttributes<HTMLUListElement> {}
 
 const Chips: FC<ChipsProps> = ({ children, className }) => {
   return <ul className={cn('flex flex-wrap gap-2', className)}>{children}</ul>
