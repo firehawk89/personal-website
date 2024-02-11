52c1965b30eed2af29c2bbfbedc099e72ffe9449
Bochkovskyi <unundertale1@ukr.net>
2024-02-11 18:28:00 +0200

    update Heading usage, utilize Chips components

--- src/components/sections/projects/project-card.tsx
+++ src/components/sections/projects/project-card.tsx
@@ -1,6 +1,8 @@
 // TODO: Add projects individual routes
 
 import Card from '@/components/ui/card'
+import Chips from '@/components/ui/chips/chips'
+import ChipsItem from '@/components/ui/chips/chips-item'
 import Heading from '@/components/ui/heading'
 import ImagePlaceholder from '@/components/ui/image-placeholder'
 import Project from '@/types/Project'
@@ -35,22 +37,19 @@ const ProjectCard: FC<ProjectCardProps> = ({
             <ImagePlaceholder />
           )}
           <div className="absolute left-0 top-0 flex h-full w-full flex-col justify-end gap-3 bg-gradient-to-t from-dark p-4 text-light opacity-0 transition-all group-hover:opacity-100 sm:gap-5 sm:px-6 sm:py-7">
-            <Heading size="h3">{title || slug}</Heading>
+            <Heading level={3} size="h3">
+              {title || slug}
+            </Heading>
             <p
               className="line-clamp-1 sm:line-clamp-2 md:line-clamp-3"
               dangerouslySetInnerHTML={{ __html: description }}
             />
             {technologies && (
-              <div className="flex flex-wrap gap-2">
+              <Chips>
                 {technologies.map((technology) => (
-                  <span
-                    className="rounded-xl bg-accent bg-opacity-75 px-2 py-1 text-sm font-semibold"
-                    key={technology}
-                  >
-                    {technology}
-                  </span>
+                  <ChipsItem key={technology} label={technology} size="sm" />
                 ))}
-              </div>
+              </Chips>
             )}
           </div>
         </Link>
