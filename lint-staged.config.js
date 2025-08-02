310521b5fb5de1355a3534137df6ae74240add65
Anton Bochkovskiy <unundertale1@ukr.net>
2025-08-02 16:12:17 +0300

    fix: update pre-commit hook and lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 export default {
-  '*.{js,jsx,ts,tsx}': (stagedFiles) => [`prettier --write ${stagedFiles.join(' ')}`, `eslint .`],
-  '*.{js,ts}': () => 'npm run typescript:check',
-  '*': 'prettier --write --ignore-unknown',
-  '*.{js,jsx,ts,tsx,md,css,scss,json}': (stagedFiles) =>
+  '*': (stagedFiles) => `prettier --write --ignore-unknown ${stagedFiles.join(' ')}`,
+  '*.{js,ts,svelte}': () => ['tsc -p tsconfig.json', 'eslint .'],
+  '*.{js,ts,svelte,md,css,scss,json}': (stagedFiles) =>
     `cspell --show-suggestions --quiet --gitignore ${stagedFiles.join(' ')}`
 }
