23cf4b7dd6b9324d54279cbf055d0c7bfe18332e
Anton Bochkovskiy <unundertale1@ukr.net>
2025-08-04 23:00:08 +0300

    feat: add iconify packages and vite-chrome-devtools package

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import { sveltekit } from '@sveltejs/kit/vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit(), tailwindcss()]
+  plugins: [enhancedImages(), sveltekit(), tailwindcss(), devtoolsJson()]
 })
